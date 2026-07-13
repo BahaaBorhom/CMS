7db6849c8f7da806833ee7ed8fcf4fd04c8505d2
shadow55899 <kidstan2@gmail.com>
2026-07-13 14:46:03 +0300

    minor change

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -335,12 +335,12 @@ class HomeScreen extends StatelessWidget {
           Expanded(
             child: GestureDetector(
               onTap: () {
-              //  Navigator.push(
-              //     context,
-              //     MaterialPageRoute(
-              //       builder: (context) => const SearchScreen(),
-              //     ),
-              //   );
+                //  Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //       builder: (context) => const SearchScreen(),
+                //     ),
+                //   );
               },
               child: Container(
                 height: 48,
@@ -811,6 +811,28 @@ class HomeScreen extends StatelessWidget {
                             width: 215,
                             height: 135,
                             fit: BoxFit.cover,
+                          ),
+                        ),
+                        IgnorePointer(
+                          child: Container(
+                            height: 135,
+                            alignment: Alignment.bottomLeft,
+                            padding: const EdgeInsets.fromLTRB(16, 0, 12, 0),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.black.withOpacity(0),
+                                  Colors.black.withOpacity(1),
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                stops: const [0.6, 1],
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(16),
+                              ),
+                            ),
+                            child: Container(),
                           ),
                         ),
                         Positioned(
